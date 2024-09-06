9e78fbbe42db67c4e980e0e1ffee8664cf8bdd26
Matheus Quevedo <matheusq10@gmail.com>
2024-09-06 15:36:15 -0300

    Criação: base de dados criada

---
+++ dados.js
@@ -0,0 +1,90 @@
+let dados = [
+    {
+        imagem: "src/mercury.png",
+        nome: "Mercúrio",
+        velocidade: "47,362 km/s",
+        periodo: "87,969 dias",
+        massa: "3,3011×10²³ kg",
+        temperatura: "166,85 ºC",
+        gravidade: "3,7 m/s²",
+        satelites: "Nenhum",
+        wiki: "https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)#:~:text=Merc%C3%BArio%20%C3%A9%20um%20dos%20quatro,Tit%C3%A3%2C%20embora%20seja%20mais%20massivo.",
+    },
+    {
+        imagem: "src/venus.png",
+        nome: "Vênus",
+        velocidade: "35,02 km/s",
+        periodo: "243 dias",
+        massa: "4,8675×10²⁴ kg",
+        temperatura: "461,85 ºC",
+        gravidade: "8,87 m/s²",
+        satelites: "Nenhum",
+        wiki: "https://pt.wikipedia.org/wiki/V%C3%AAnus_(planeta)"
+    },
+    {
+        imagem: "src/earth.png",
+        nome: "Terra",
+        velocidade: "29,78 km/s",
+        periodo: "1 dia",
+        massa: "5,972×10²⁴ kg",
+        temperatura: "15 ºC",
+        gravidade: "9,8 m/s²",
+        satelites: "1",
+        wiki: "https://pt.wikipedia.org/wiki/Terra"
+    },
+    {
+        imagem: "src/mars.png",
+        nome: "Marte",
+        velocidade: "24,077 km/s",
+        periodo: "1,025 dias",
+        massa: "6,4171×10²³ kg",
+        temperatura: "-63 ºC",
+        gravidade: "3,71 m/s²",
+        satelites: "2",
+        wiki: "https://pt.wikipedia.org/wiki/Marte_(planeta)"
+    },
+    {
+        imagem: "src/jupiter.png",
+        nome: "Júpiter",
+        velocidade: "13,07 km/s",
+        periodo: "4 331,572 dias",
+        massa: "1,8986×10²⁷ kg",
+        temperatura: "-108 ºC",
+        gravidade: "24,79 m/s²",
+        satelites: "95",
+        wiki: "https://pt.wikipedia.org/wiki/J%C3%BApiter_(planeta)"
+    },
+    {
+        imagem: "src/saturn.png",
+        nome: "Saturno",
+        velocidade: "9,68 km/s",
+        periodo: "1 075 922 dias",
+        massa: "5,6834×10²⁶ kg",
+        temperatura: "-139 ºC",
+        gravidade: "10,44 m/s²",
+        satelites: "146",
+        wiki: "https://pt.wikipedia.org/wiki/Saturno_(planeta)"
+    },
+    {
+        imagem: "src/uranus.png",
+        nome: "Urano",
+        velocidade: "6,81 km/s",
+        periodo: "30 799,095 dias",
+        massa: "8,6810×10²⁵ kg",
+        temperatura: "-195 ºC",
+        gravidade: "8,69 m/s²",
+        satelites: "28",
+        wiki: "https://pt.wikipedia.org/wiki/Urano_(planeta)"
+    },
+    {
+        imagem: "src/neptune.png",
+        nome: "Netuno",
+        velocidade: "5,43 km/s",
+        periodo: "60 190,03 dias",
+        massa: "1,0243×10²⁶ kg",
+        temperatura: "-223 ºC",
+        gravidade: "11,15 m/s²",
+        satelites: "14",
+        wiki: "https://pt.wikipedia.org/wiki/Netuno_(planeta)"
+    }
+];
